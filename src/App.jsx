2c6d45e7a277282a01a2b5c028e3d62c4f7de1e1
anurag <guptaanurag5395@gmail.com>
2019-12-30 23:25:53 +0530

    delete additional files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <nav class="navbar navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          Hello
+        </a>
+      </nav>
+    );
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import React from "react";
+import ReactDom from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDom.render(<App />, document.getElementById("root"));
